1046b327e7b40be6b98c99e5ace009910ccd3aeb
prade <prade@DESKTOP-2BS24HJ>
2022-04-27 18:11:58 +0530

    my first git staging

---
+++ src/test/java/testdata/Tes1.java
@@ -0,0 +1,12 @@
+package testdata;
+
+public class Tes1 {
+
+	public static void main(String[] args) {
+		String x= "asjhjkfhi45&%^jsdo83nf09$";
+		String y = x.replaceAll("[^0-9]","");
+		System.out.println(y);
+
+	}
+
+}
