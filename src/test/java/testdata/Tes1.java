595cb61e9c55c27da9d2db906e46db88b49a4bc2
PradeepManager <104506978+PradeepManager@users.noreply.github.com>
2022-04-27 18:58:30 +0530

    Update Tes1.java
    change-1

--- src/test/java/testdata/Tes1.java
+++ src/test/java/testdata/Tes1.java
@@ -3,7 +3,7 @@ package testdata;
 public class Tes1 {
 
 	public static void main(String[] args) {
-		String x= "asjhjkfhi45&%^jsdo83nf09$";
+		String x= "asjhjkfhi45&%^jsdo83nf09$AbdulKalam";
 		String y = x.replaceAll("[^0-9]","");
 		System.out.println(y);
 
